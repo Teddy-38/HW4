053c5f450024587a092cb8d32439e4f2c181d40c
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-09 21:16:15 +0800

    task2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,10 @@ public class Main {
         } else {
             System.out.println("Возраст совершеннолетия еще не наступил, нужно подождать еще немного");}
         System.out.println("Task 2");
+        int temp = 8;
+        if (temp < 5) {
+            System.out.println("На улице " +temp+ " градуса, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " +temp+ " градусов, можно идти без шапки");}
     }
 }
